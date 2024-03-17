8734248f865168b3f46b41873c1c9269dab55e91
3636382e35 <jtuyay10@gmail.com>
2024-03-17 21:45:57 +0800

    .

---
+++ cmus/lib.pl
@@ -0,0 +1,48 @@
+/home/d4r10/Music/A Fine Red Mist.mp3
+/home/d4r10/Music/All for Now.mp3
+/home/d4r10/Music/A Tense Moment.mp3
+/home/d4r10/Music/At Home.mp3
+/home/d4r10/Music/Blue Room (Kz-Version).mp3
+/home/d4r10/Music/Breath of a Serpent.mp3
+/home/d4r10/Music/Chinatown.mp3
+/home/d4r10/Music/Coming Down.mp3
+/home/d4r10/Music/Delusive Bunker.mp3
+/home/d4r10/Music/devil's kiss - breakcore mix.mp3
+/home/d4r10/Music/Driving Force： Neon Fog.mp3
+/home/d4r10/Music/₊˚✩ emotional⧸nostalgic breakcore playlist ☾ ⁺₊.mp3
+/home/d4r10/Music/End of the Road.mp3
+/home/d4r10/Music/Full Confession.mp3
+/home/d4r10/Music/Hit the Floor.mp3
+/home/d4r10/Music/I love you so - The Walters (cover).mp3
+/home/d4r10/Music/Katana Zero (Ost-Version).mp3
+/home/d4r10/Music/Kill Your TV.mp3
+/home/d4r10/Music/Kiss me (sped up).mp3
+/home/d4r10/Music/Mabisyo - In Search Of Freedom.mp3
+/home/d4r10/Music/Mabisyo - Sun Colored Eyes.mp3
+/home/d4r10/Music/Mabisyo - The Funeral Of The Flowers.mp3
+/home/d4r10/Music/Macroblank - keygen 決定的.mp3
+/home/d4r10/Music/Macroblank - serpent lp.mp3
+/home/d4r10/Music/Macroblank - ごめんと言って lp.mp3
+/home/d4r10/Music/Macroblank - 絶望に負けた.mp3
+/home/d4r10/Music/Male Alchemy - 魂の憎しみの本質.mp3
+/home/d4r10/Music/Meat Grinder.mp3
+/home/d4r10/Music/Men at Work - Who Can It Be Now (Boogie Hill Faders Remix).mp3
+/home/d4r10/Music/Nabbe - Distance.mp3
+/home/d4r10/Music/Nightcore - Anxiety (Lyrics) [blackbear & FRND].mp3
+/home/d4r10/Music/Nocturne (Co-Composed by Justin Stander).mp3
+/home/d4r10/Music/Overdose.mp3
+/home/d4r10/Music/Panoramic Feelings.mp3
+/home/d4r10/Music/Prison Air 1.mp3
+/home/d4r10/Music/Prison Air 2.mp3
+/home/d4r10/Music/Rain on Brick.mp3
+/home/d4r10/Music/Silhouette.mp3
+/home/d4r10/Music/Sneaky Driver.mp3
+/home/d4r10/Music/Snow.mp3
+/home/d4r10/Music/The Sandman 1.mp3
+/home/d4r10/Music/The Sandman 2.mp3
+/home/d4r10/Music/Third District.mp3
+/home/d4r10/Music/Tied up at the Moment.mp3
+/home/d4r10/Music/V Limo (New Donk Shitty).mp3
+/home/d4r10/Music/Volition.mp3
+/home/d4r10/Music/Worst Neighbors Ever.mp3
+/home/d4r10/Music/You Will Never Know.mp3
---
+++ i3/scripts/rkeys.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Command to execute with sudo
+evremap remap ~/.config/remapping-keys.toml
